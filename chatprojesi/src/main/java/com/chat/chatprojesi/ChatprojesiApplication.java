d62f816468a2c58a5fc0254e52a5b4f534741d1f
sehedkaddur <Sehedkaddur@ogrenci.beykoz.edu.tr>
2026-05-22 22:42:12 +0300

    Kullanıcı sistemi oluşturuldu

---
+++ chatprojesi/src/main/java/com/chat/chatprojesi/ChatprojesiApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chatprojesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatprojesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatprojesiApplication.class, args);
+	}
+
+}
---
+++ chatprojesi/src/main/java/model/User.java
@@ -0,0 +1,34 @@
+package model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String kullaniciAdi;
+    private String sifre;
+    private String eposta;
+    public Long getId() {
+        return id;}
+    public void setId(Long id) {
+        this.id = id;}
+    public String getKullaniciAdi() {
+        return kullaniciAdi;}
+    public void setKullaniciAdi(String kullaniciAdi) {
+        this.kullaniciAdi = kullaniciAdi;}
+    public String getSifre() {
+        return sifre;
+    }
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+    public String getEposta() {
+        return eposta;
+    }
+    public void setEposta(String eposta) {
+        this.eposta = eposta;
+    }
+}
