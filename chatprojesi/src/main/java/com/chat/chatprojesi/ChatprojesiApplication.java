aceb258f7b835ce36e1ecbcbc5d909129a957e36
sehedkaddur <Sehedkaddur@ogrenci.beykoz.edu.tr>
2026-05-23 12:46:25 +0300

    Kullanıcı sistemi oluşturuldu

--- chatprojesi/src/main/java/com/chat/chatprojesi/ChatprojesiApplication.java
+++ chatprojesi/src/main/java/com/chat/chatprojesi/ChatprojesiApplication.java
@@ -2,8 +2,11 @@ package com.chat.chatprojesi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("model")
+
 public class ChatprojesiApplication {
 
 	public static void main(String[] args) {
